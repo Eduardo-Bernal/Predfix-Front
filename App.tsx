3f7c35452aa6bd6b94fe0c463322fac3f595b695
Eduardo-Bernal <cbedudu@gmail.com>
2026-08-10 10:35:54 -0300

    Feat: AreaProvider

--- App.tsx
+++ App.tsx
@@ -1,13 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
+import Routes from "./src/routes";
+import {SafeAreaProvider, SafeAreaView} from "react-native-safe-area-context";
+import {StatusBar} from "react-native";
 import React from "react";
-import {Text, View } from 'react-native';
 
 export default function App() {
-  return (
-    <View>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <SafeAreaProvider>
+            <SafeAreaView style={{flex: 1}}>
+                <Routes />
+            </SafeAreaView>
+        </SafeAreaProvider>
+    )
 }
-
