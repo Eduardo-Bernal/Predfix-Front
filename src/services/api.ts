a5e4436c86d52d19e1b6ef4af2ec592318d86d66
LUCAS RODRIGUES RIVOLTA <46377500831@info.local>
2026-08-31 10:00:43 -0300

    consumo detalhes

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,17 +1,26 @@
-import axios from "axios";
-import secureLocalStorage from "react-secure-storage";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import axios from "axios"
+import { Platform } from "react-native";
 
-const apiLocal = "https://localhost:7197/api/"
+//definindo o host local conforme plataforma(expo, web, ios...)
+const host = Platform.OS === 'android' ? '10.0.2.2' : 'localhost';
+const porta = process.env.EXPO_PUBLIC_PORTA;
+//dessa forma, conseguimos rodar tanto na web quanto no emulador
+const enderecoApi = process.env.EXPO_PUBLIC_API_URL || `http://${host}:${porta}`;
 
 export const api = axios.create({
-    baseURL: apiLocal,
-})
+    baseURL: enderecoApi,
+    timeout: 10000
+});
 
-api.interceptors.request.use((config)=> {
-    const token = secureLocalStorage.getItem("token")
+//SOLICITAÇÃO/REQUISIÇÃO --OPAAA PERA AI, QUERO O MEU TOKEN DO USUÁRIO-->
+//interceptar/impedir o curso toda requisição feita pela API
+api.interceptors.request.use(async (config) =>{
+    const token = await AsyncStorage.getItem(process.env.EXPO_PUBLIC_TOKEN_KEY);
 
     if(token){
-        config.headers.Authorization = `Bearer ${token}`
+        //configurar o Bearer
+        config.headers.Authorization = "Bearer " + token
     }
 
     return config;
